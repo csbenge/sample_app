6862b95b8146dbf3ad07f62b8d2a56d98c213a6c
Carey Benge <csbenge@gmail.com>
2022-06-27 14:49:54 -0500

    Add password resets & email configuration

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -18,11 +18,15 @@ class UserMailerTest < ActionMailer::TestCase
   
 
   test "password_reset" do
-    mail = UserMailer.password_reset
+    @user.activation_token = User.new_token
+    @user.reset_token = User.new_token
+    mail = UserMailer.password_reset(@user)
     assert_equal "Password reset", mail.subject
-    assert_equal ["to@example.org"], mail.to
+    assert_equal ["tuser1@example.com"], mail.to
     assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
+    #assert_match "Hi", mail.body.encoded
+    assert_match @user.reset_token,        mail.body.encoded
+    assert_match CGI::escape(@user.email), mail.body.encoded
   end
 
 end
